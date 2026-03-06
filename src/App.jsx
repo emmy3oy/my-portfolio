ede32f71c7ec2efee71503ebd6ed10c8afe07be7
Chapo <chapordp400@outlook.com>
2026-03-06 06:44:32 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <Router>
       <NavigationBar />
-
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
