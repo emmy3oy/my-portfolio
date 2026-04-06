f5f8b15ce33edd12f220cb3bad83dd349afab550
Chapo <chapordp400@outlook.com>
2026-04-06 09:16:44 -0400

    footer commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,32 @@
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
 function Footer() {
   return (
-    <div className="text-center mt-5 p-4">
-      © 2026 Emmanuel | React Portfolio
-    </div>
+    <footer className="text-center mt-5 p-4 bg-dark text-light">
+      <div className="mb-3">
+        <a
+          href="https://github.com/emmy3oy"
+          target="_blank"
+          rel="noreferrer"
+          className="text-light mx-3 fs-4"
+        >
+          <FaGithub />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/emmanuel-oyase-4b709b280/"
+          target="_blank"
+          rel="noreferrer"
+          className="text-light mx-3 fs-4"
+        >
+          <FaLinkedin />
+        </a>
+      </div>
+
+      <p className="mb-0">
+        © {new Date().getFullYear()} Emmanuel | React Portfolio
+      </p>
+    </footer>
   );
 }
 
